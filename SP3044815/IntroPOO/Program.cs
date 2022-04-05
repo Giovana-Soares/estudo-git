f0ef4a4fa035f4352aeb053835e136815336335a
Giovana-Soares <101850362+Giovana-Soares@users.noreply.github.com>
2022-04-05 15:28:04 -0300

    Add files via upload

---
+++ SP3044815/IntroPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Quadrado");
+
+Quadrado quadrado1 = new Quadrado(2.0);
+Quadrado quadrado2 = new Quadrado(4.0);
+
+Console.WriteLine(quadrado1.Perimetro);
+Console.WriteLine(quadrado1.Area);
+Console.WriteLine(quadrado1.Lado);
+
+Console.WriteLine("Retângulo");
+Retangulo ret1= new Retangulo(2.0,4.0);
+Retangulo ret2= new Retangulo(3.0,6.0);
+
+Console.WriteLine(ret1.Perimetro);
+Console.WriteLine(ret1.Area);
